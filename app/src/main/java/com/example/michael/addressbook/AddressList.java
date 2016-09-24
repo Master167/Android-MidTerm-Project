9125cb9f22fe2f22873d85e78ad3f7ff94fc625c
Michael Frederick <master16789@gmail.com>
2016-09-23 21:19:20 -0400

    Setup First Activity Menu and such
    Text Color is wierd though....

--- app/src/main/java/com/example/michael/addressbook/AddressList.java
+++ app/src/main/java/com/example/michael/addressbook/AddressList.java
@@ -2,12 +2,40 @@ package com.example.michael.addressbook;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class AddressList extends AppCompatActivity {
+    private ListView listView;
+    private ArrayAdapter<ListView> listViewArrayAdapter;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_address_list);
+        toolbar = (Toolbar) findViewById(R.id.address_toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_item, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_add) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
