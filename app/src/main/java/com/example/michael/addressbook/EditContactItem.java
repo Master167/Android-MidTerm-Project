d56021a68b5ca44cbbfa7e006ed75605a17fe689
Michael Frederick <master16789@gmail.com>
2016-09-25 17:08:20 -0400

    Build out edit contact list activtiy a little
    Left notes for what needs to be done

--- app/src/main/java/com/example/michael/addressbook/EditContactItem.java
+++ app/src/main/java/com/example/michael/addressbook/EditContactItem.java
@@ -6,15 +6,32 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class EditContactItem extends AppCompatActivity {
+    TextView nameRequiredMessage;
+    EditText editName;
+    EditText editPhone;
+    EditText editEmail;
+    EditText editStreet;
+    EditText editCity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_contact_item);
 
+        // Get EditTexts
 
+        // Get nameRequire TextView
+
+        // Setup Save Contact Button
+
+        // Validate that Name is entered
+
+        // Clear nameRequired textView
     }
 
+    // Build out stuff to transfer back to main activity
 }
